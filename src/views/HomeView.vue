b7fad2f0181935974d489f507592c2994f79fcf1
marjorieabanto <102543841+marjorieabanto@users.noreply.github.com>
2023-05-06 11:22:22 -0500

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
+import FilterContent from "@/components/filter-content.component.vue";
+import SearchBarContent from "@/components/search-bar-content.vue";
+import ResultContent from "@/components/result-content.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <search-bar-content />
+    <div class="flex flex-wrap justify-content-between w-10 mx-auto mt-4">
+      <FilterContent />
+      <Result-content />
+    </div>
   </main>
 </template>
+
