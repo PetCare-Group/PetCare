68815212270cc2d6e8fa344180f3370db5fceb2d
Yamathe5-laptop <manuelsegmar@gmail.com>
2023-06-23 13:09:01 -0500

    ADD: info

---
+++ src/components/payment-form-component.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="payment-form">
+      <h2>Ingrese los detalles de pago</h2>
+  
+      <div class="form-group">
+        <label for="cardNumber">Número de tarjeta</label>
+        <input type="text" id="cardNumber" v-model="cardNumber" placeholder="Número de tarjeta" />
+      </div>
+  
+      <div class="form-group">
+        <label for="firstName">Nombre</label>
+        <input type="text" id="firstName" v-model="firstName" placeholder="Nombre" />
+      </div>
+  
+      <div class="form-group">
+        <label for="lastName">Apellido</label>
+        <input type="text" id="lastName" v-model="lastName" placeholder="Apellido" />
+      </div>
+  
+      <div class="form-group">
+        <label for="expirationDate">Fecha de expiración</label>
+        <input type="text" id="expirationDate" v-model="expirationDate" placeholder="MM/AA" />
+      </div>
+  
+      <div class="form-group">
+        <label for="cvv">CVV</label>
+        <input type="text" id="cvv" v-model="cvv" placeholder="CVV" />
+      </div>
+  
+      <div class="form-actions">
+        <button @click="submitForm">Realizar pago</button>
+        <button @click="closeForm">Cancelar</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        cardNumber: '',
+        firstName: '',
+        lastName: '',
+        expirationDate: '',
+        cvv: '',
+      };
+    },
+    methods: {
+      submitForm() {
+        // Lógica para procesar el pago
+        // Puedes enviar los datos a un servidor o realizar validaciones adicionales aquí
+        console.log('Formulario de pago enviado');
+      },
+      closeForm() {
+        this.$emit('close'); // Emitir evento para cerrar el formulario
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .payment-form {
+    width: 300px;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 5px;
+  }
+  
+  .payment-form h2 {
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+  
+  .form-group {
+    margin-bottom: 15px;
+  }
+  
+  .form-group label {
+    display: block;
+    font-size: 14px;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+  
+  .form-group input {
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  
+  .form-actions {
+    margin-top: 20px;
+  }
+  
+  .form-actions button {
+    margin-right: 10px;
+    padding: 8px 16px;
+    background-color: #FBB847;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  </style>
+  
---
+++ src/views/reserva-exito.vue
@@ -0,0 +1,77 @@
+<script setup>
+import HeaderContent from "@/components/header-content.component.vue";
+import FooterContent from "@/components/footer-content.component.vue";
+import { ref } from 'vue';
+
+import PaymentForm from "@/components/payment-form-component.vue"; 
+let showPaymentForm = ref(false); // Declarar una variable reactiva para controlar la visibilidad del formulario de pago
+</script>
+
+<template>
+  <HeaderContent />
+
+  <div class="confirmation-container">
+    <img src="src/assets/logo.svg" alt="img1">
+    <h1>Tu reserva se realizó con éxito</h1>
+    <p>Podrás visualizar la información de tu reserva en Mis Servicios.</p>
+    <pv-button
+      severity="warning"
+      :style="{
+        'display': 'flex',
+        'margin-top': '25px',
+        'border-radius': '7px',
+        'justify-content': 'center',
+        'color': 'white',
+      }"
+      @click="showPaymentForm = true"
+    >Volver al inicio</pv-button>
+  </div>
+
+  <!-- Capa flotante del formulario de pago -->
+  <div class="payment-form-overlay" v-if="showPaymentForm">
+    <PaymentForm @close="showPaymentForm = false" /> <!-- Pasar un evento "close" para cerrar el formulario -->
+  </div>
+
+  <!-- <FooterContent /> -->
+</template>
+
+<style scoped>
+.confirmation-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin-top: 50px;
+}
+
+h1 {
+  font-weight: bold;
+  font-size: 30px;
+  color: #FBB847;
+  margin-top: 20px;
+}
+
+p {
+  font-size: 18px;
+  margin-top: 10px;
+}
+
+img {
+  width: 200px;
+  height: 200px;
+  border-radius: 100px;
+}
+
+.payment-form-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+}
+</style>
